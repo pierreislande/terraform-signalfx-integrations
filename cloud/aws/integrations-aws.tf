c3b532ff417ba78440def837239cb0f17ca98a7a
Quentin Manfroi <quentin.manfroi@fr.clara.net>
2020-02-27 13:05:09 +0100

    improve inputs description and typing

--- cloud/aws/integrations-aws.tf
+++ cloud/aws/integrations-aws.tf
@@ -3,15 +3,15 @@ resource "signalfx_aws_external_integration" "aws_claranet_external" {
 }
 
 resource "signalfx_aws_integration" "aws_claranet" {
-	enabled = var.enable_flag
+	enabled = var.enabled
 
 	integration_id = signalfx_aws_external_integration.aws_claranet_external.id
 	external_id = signalfx_aws_external_integration.aws_claranet_external.external_id
 	role_arn = aws_iam_role.aws_sfx_role.arn
 	regions = var.aws_regions
-	poll_rate = var.aws_poll_rate
-	import_cloud_watch = var.import_cloud_watch_flag
-	enable_aws_usage = var.enable_aws_usage_flag
+	poll_rate = var.poll_rate
+	import_cloud_watch = var.import_cloudwatch
+	enable_aws_usage = var.import_aws_usage
 
 	/*custom_namespace_sync_rule {
 		default_action = var.custom_namespace_rules_default_action
